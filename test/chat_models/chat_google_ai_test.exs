e85ba6213e5bd2fd772ecbaae4d555579e5d0a96
Jinkyou Son <nallwhy@gmail.com>
2025-08-24 20:50:24 +0900

    Handle no content responses in ChatGoogleAI

--- test/chat_models/chat_google_ai_test.exs
+++ test/chat_models/chat_google_ai_test.exs
@@ -524,6 +524,25 @@ defmodule ChatModels.ChatGoogleAITest do
       assert call.arguments == %{"value" => 123}
     end
 
+    test "handles no parts in content", %{model: model} do
+      response = %{
+        "candidates" => [
+          %{
+            "content" => %{
+              "role" => "model"
+            },
+            "finishReason" => "STOP",
+            "index" => 0
+          }
+        ]
+      }
+
+      assert [%Message{} = struct] = ChatGoogleAI.do_process_response(model, response)
+      assert struct.role == :assistant
+      assert struct.content == []
+      assert struct.status == :complete
+    end
+
     test "handles receiving MessageDeltas as well", %{model: model} do
       response = %{
         "candidates" => [
